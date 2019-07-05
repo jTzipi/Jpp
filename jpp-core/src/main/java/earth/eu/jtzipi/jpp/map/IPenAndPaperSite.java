caf8479865472321d3c502007301f832799e12dd
jTzipi <tlhammer@mailbox.org>
2019-07-05 16:34:41 +0200

    Gadi

--- jpp-core/src/main/java/earth/eu/jtzipi/jpp/map/IPenAndPaperSite.java
+++ jpp-core/src/main/java/earth/eu/jtzipi/jpp/map/IPenAndPaperSite.java
@@ -1,7 +1,23 @@
+/*
+ *    Copyright 2019 (c) Tim Langhammer
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
 package earth.eu.jtzipi.jpp.map;
 
 import java.util.List;
-import java.util.Set;
+
 
 /**
  * Pen and Paper Site.
@@ -11,6 +27,10 @@ import java.util.Set;
  */
 public interface IPenAndPaperSite {
 
+    /**
+     *
+     * @return
+     */
     String getName();
 
     List<IPenAndPaperMap> getLevelMaps();
