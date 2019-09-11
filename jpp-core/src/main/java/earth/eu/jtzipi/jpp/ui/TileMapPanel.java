ccb9b358b742130463010222c82dbd8012f997c2
jTzipi <tlhammer@mailbox.org>
2019-09-11 17:59:17 +0200

    Map Panel update

--- jpp-core/src/main/java/earth/eu/jtzipi/jpp/ui/TileMapPanel.java
+++ jpp-core/src/main/java/earth/eu/jtzipi/jpp/ui/TileMapPanel.java
@@ -4,16 +4,13 @@ import earth.eu.jtzipi.jpp.ui.tile.PenAndPaperPos;
 import earth.eu.jtzipi.jpp.ui.tile.segment.ISegment;
 import earth.eu.jtzipi.jpp.ui.tile.segment.PathBuilder;
 import earth.eu.jtzipi.jpp.ui.tile.segment.Wall;
-import javafx.beans.property.DoubleProperty;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
 import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.Stop;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Shape;
+import javafx.scene.shape.*;
 
 
 /**
@@ -35,25 +32,24 @@ public class TileMapPanel extends Pane {
 
         setPrefHeight( h );
         setPrefWidth( w );
-        Rectangle tileBG = new Rectangle( w, 50 );
-        tileBG.setLayoutX( 0D );
+        Rectangle tileBG = new Rectangle( w - 100D, 50 );
+        tileBG.setLayoutX( 50D );
 
 
         Rectangle clipFadeWest = new Rectangle( 50, 50 );
-        clipFadeWest.setLayoutX( 0D );
+        clipFadeWest.setLayoutX( 50D );
 
         Rectangle clipFadeEast = new Rectangle( 50, 50 );
-        clipFadeEast.setLayoutX( w - 50D );
+        clipFadeEast.setLayoutX( w - 100D );
 
 
-        //Shape clip = Shape.union( tileScroller, clipFadeEast );
-        //clip = Shape.union( clip, clipFadeWest );
+
 
         Color bgMain = Color.rgb( 255, 255, 255, 0.5D );
         Color bgTopBottom = Color.rgb( 0, 0, 75, 0.75D );
 
         Color gcOpaque = Color.rgb( 10, 10, 75, 1D );
-        Color gcTransparent = Color.rgb( 10, 10, 75, 0.1D );
+        Color gcTransparent = Color.rgb( 10, 10, 75, 0D );
 
         LinearGradient westFade = new LinearGradient( 0D, 0D, 1D, 0D, true, CycleMethod.NO_CYCLE, new Stop( 0D, gcOpaque ), new Stop( 1D, gcTransparent ) );
 
@@ -71,10 +67,17 @@ public class TileMapPanel extends Pane {
         clipFadeEast.setFill( eastFade );
 
         clipFadeWest.setFill( westFade );
+        Group tilePane = createTilePane();
+        Rectangle clip = new Rectangle( w - 100D, h );
+        clip.setLayoutX( 50D );
+        tilePane.setClip( clip );
 
-
-        getChildren().addAll( tileBG, createTilePane(), ScrollKnob.ofWest( 50, 50 ), clipFadeWest );
+        ScrollKnob wknob = ScrollKnob.of( PenAndPaperPos.W, 50D, 50D );
+        ScrollKnob eKnob = ScrollKnob.of( PenAndPaperPos.E, 50D, 50D );
+        eKnob.setLayoutX( w - 50 );
+        getChildren().addAll( wknob, tileBG, tilePane, clipFadeWest, clipFadeEast, eKnob );
     }
+
 
 
     private Group createTilePane() {
@@ -82,7 +85,8 @@ public class TileMapPanel extends Pane {
         Group group = new Group();
 
 
-        double xoff = -50D;
+        double xoff = 0D;
+
         for ( ISegment w : Wall.WallSegments.values() ) {
             Shape shape = w.createPath();
             if ( shape != null ) {
@@ -91,8 +95,11 @@ public class TileMapPanel extends Pane {
                 shape.setStroke( Color.RED );
                 shape.setScaleY( 2D );
 
-
+                Line sepa = new Line( xoff, 0D, xoff, 50D );
+                sepa.setStrokeWidth( 0.5D );
+                sepa.setStroke( Color.rgb( 0, 0, 75, 0.75D ) );
                 group.getChildren().add( shape );
+                group.getChildren().add( sepa );
                 xoff += 50;
             }
         }
@@ -100,41 +107,105 @@ public class TileMapPanel extends Pane {
         return group;
     }
 
+    /**
+     * A scroll knob control.
+     */
     public static class ScrollKnob extends Group {
 
-        DoubleProperty fxWidthProp;
-        DoubleProperty fxHeightProp;
-        Shape arrow;
-        Rectangle bg;
-        PenAndPaperPos ppp;
+        public static final double WIDTH_DEF = 50D;
+        public static final double HEIGHT_DEF = 50D;
+        Rectangle bg;               // knob background
+        Arc arc;                    // arc border
+        PenAndPaperPos ppp;         // position
+        Color fill = Color.rgb( 10, 10, 75, 1D );
+        Color arrowNormal = Color.rgb( 60, 60, 125, 1D );
+        Color arrowHighlight = Color.LIGHTBLUE;
+        private double w;           // width
+        private double h;           // height
+        private Shape arrow;                // arrow
+
 
         ScrollKnob( PenAndPaperPos paperPos, double width, double height ) {
-            this.fxHeightProp = new SimpleDoubleProperty( this, "", height );
-            this.fxWidthProp = new SimpleDoubleProperty( this, "", width );
-
+            this.w = width;
+            this.h = height;
+            this.ppp = paperPos;
             createScrollKnob();
         }
 
-        static ScrollKnob ofWest( double w, double h ) {
+        /**
+         * Create a knob for position.
+         *
+         * @param penAndPaperPos pos
+         * @param w              width
+         * @param h              height
+         * @return scroll knob
+         */
+        static ScrollKnob of( PenAndPaperPos penAndPaperPos, double w, double h ) {
 
-            return new ScrollKnob( PenAndPaperPos.W, w, h );
+            return new ScrollKnob( penAndPaperPos, w, h );
+        }
+
+        static ScrollKnob of( PenAndPaperPos penAndPaperPos ) {
+
+            return of( penAndPaperPos, ScrollKnob.WIDTH_DEF, ScrollKnob.HEIGHT_DEF );
         }
 
         void createScrollKnob() {
 
-            bg = new Rectangle();
-            bg.widthProperty().bind( fxWidthProp );
-            bg.heightProperty().bind( fxHeightProp );
-            arrow = PathBuilder.create().mxy( 46D, 5D ).lxy( 10D, 25D ).lxy( 46D, 45D ).build();
-            arrow.setFill( Color.LIGHTBLUE );
 
-            Color gcOpaque = Color.rgb( 10, 10, 75, 1D );
-            Color gcTransparent = Color.rgb( 10, 10, 75, 0.1D );
+            double mid = w / 2D;
+            double halfmid = mid / 2D;
+            double dist = 12D;      // offset
+            double xUp = w - dist;
+            double yUp = dist;
+            double yMid = mid;
+            double xMid = dist;
+            double xDown = xUp;
+            double yDown = h - dist;
 
-            LinearGradient westFade = new LinearGradient( 0D, 0D, 1D, 0D, true, CycleMethod.NO_CYCLE, new Stop( 0D, gcOpaque ), new Stop( 1D, gcTransparent ) );
+            // create arrow
+            arrow = PathBuilder.create().mxy( xUp, yUp ).lxy( yMid, yMid ).lxy( xDown, yDown ).strokeWidth( 4D ).strokeJoin( StrokeLineJoin.ROUND ).build();
+            arrow.setStroke( Color.LIGHTBLUE );
+            // background
+            bg = new Rectangle( w - halfmid, h );
+            bg.setFill( fill );
+            // rounded border
+            arc = new Arc( dist, mid, dist, mid, 90, 180D );
+            arc.setFill( fill );
+            // rotate
+            switch ( ppp ) {
 
-            bg.setFill( westFade );
-            getChildren().addAll( bg, arrow );
+                case E:
+                    bg.setLayoutX( 0D );
+                    arc.setRotate( 180D );
+                    arc.setLayoutX( w - halfmid );
+                    arrow.setRotate( 180D );
+                    break;
+
+                case W:
+                    // set layout +1 because of 1 pixel gap between arc and bg
+                    arc.setLayoutX( 0D );
+                    bg.setLayoutX( halfmid );
+                    arrow.setTranslateX( -halfmid );
+                    break;
+                case S:
+                    arc.setRotate( 90D );
+                    bg.setLayoutY( 0D );
+                    arc.setLayoutY( h - halfmid );
+                    arrow.setRotate( 90D );
+                    break;
+                case N:
+                    arc.setRotate( 270D );
+                    bg.setLayoutY( mid );
+                    arc.setLayoutY( dist );
+                    arrow.setRotate( 270D );
+                    break;
+                default:
+                    break;
+            }
+
+
+            getChildren().addAll( arc, bg, arrow );
         }
     }
 }
